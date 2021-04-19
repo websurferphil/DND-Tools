96bacdd54b52ce8ecab65b636240abbcf846a844
websurferphil <p_dijo@outlook.com>
2021-04-18 17:40:58 -0700

    Switched to Eel for better looking program

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # Importing modules
 from config import Config
 import utils
-from classes import *
 
-import tkinter as tk
-from tkinter import ttk
+import eel
 
 # Importing the menu items
 
@@ -14,28 +12,19 @@ import npc.npc as npc
 # Set up
 
 dndconf = Config.MENU_OPTIONS
-root = tk.Tk()
-root.minsize(800, 600)
-root.columnconfigure(10, weight=1, minsize=80)
-root.columnconfigure(10, weight=1, minsize=60)
-root.title("DND-Tools by WebSurferPhil")
-tab_ctrl = ttk.Notebook(root)
+
+
+eel.init('web')
+eel.start('main.html', geometry={'size': (200, 100), 'position': (300, 50)})
+
 
 dndconf["dice"] = utils.conf("Dice Rollers", dice, dice.options)
 dndconf["npc"] = utils.conf("NPC Generators", npc, npc.options)
 
-dice_frame = tk.Frame(tab_ctrl, bg="blue")
-npc_frame = tk.Frame(tab_ctrl, bg="red")
-
-dice_one = EntryNeeded(dice_frame, dndconf["dice"]["options"][0]["function"])
 
 def main():
-    tab_ctrl.add(dice_frame, text=dndconf["dice"]["name"])
-    tab_ctrl.add(npc_frame, text=dndconf["npc"]["name"])
 
-    tab_ctrl.pack(expand=1, fill="both")
-
-    root.mainloop()
+    print("something")
 
 
 if __name__ == "__main__":
